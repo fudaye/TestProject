c8fa79d2bc676d3cec24c7c493bdfecab0e70a2d
福大爷 <zhaofuyue@gmail.com>
2021-08-20 11:38:00 +0800

    add internet call

--- RetrofitDemo/app/src/main/java/com/example/retrofitdemo/MainActivity.kt
+++ RetrofitDemo/app/src/main/java/com/example/retrofitdemo/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.example.retrofitdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val retrofit = Retrofit.Builder()
+                .baseUrl("https://api.github.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+        val service = retrofit.create(GithubService::class.java)
+
+        val repos: Call<List<Repo>> = service.listRepos("octocat")
+        repos.enqueue(object : Callback<List<Repo>>{
+            override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
+                println("repos name :${response.body()!![0].full_name}")
+            }
+
+            override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
+            }
+
+        })
 
 
     }
