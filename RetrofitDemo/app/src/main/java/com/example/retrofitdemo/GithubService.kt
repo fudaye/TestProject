4ca08b095c5461f7a60f6314b18be93c58e61360
福大爷 <zhaofuyue@gmail.com>
2021-08-20 11:16:00 +0800

    test

---
+++ RetrofitDemo/app/src/main/java/com/example/retrofitdemo/GithubService.kt
@@ -0,0 +1,12 @@
+package com.example.retrofitdemo
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+
+interface GithubService {
+
+    @GET("users/{user}/repos")
+    fun listRepos(@Path("user") user: String?): Call<List<Repo>>
+}
